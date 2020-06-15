1b4637c6f5fd57dcfaf1532930fafe9be7fdb860
kristof.csefalvay@gmail.com <kristof.csefalvay@gmail.com>
2020-06-15 17:30:06 +0200

    SettlersEngine completed

--- src/hu/progmasters/settlers/engine/SettlersEngine.java
+++ src/hu/progmasters/settlers/engine/SettlersEngine.java
@@ -1,9 +1,9 @@
 /*
  * Copyright © Progmasters (QTC Kft.), 2016-2018.
- * All rights reserved. No part or the whole of this Teaching Material (TM) may be reproduced, copied, distributed, 
- * publicly performed, disseminated to the public, adapted or transmitted in any form or by any means, including 
- * photocopying, recording, or other electronic or mechanical methods, without the prior written permission of QTC Kft. 
- * This TM may only be used for the purposes of teaching exclusively by QTC Kft. and studying exclusively by QTC Kft.’s 
+ * All rights reserved. No part or the whole of this Teaching Material (TM) may be reproduced, copied, distributed,
+ * publicly performed, disseminated to the public, adapted or transmitted in any form or by any means, including
+ * photocopying, recording, or other electronic or mechanical methods, without the prior written permission of QTC Kft.
+ * This TM may only be used for the purposes of teaching exclusively by QTC Kft. and studying exclusively by QTC Kft.’s
  * students and for no other purposes by any parties other than QTC Kft.
  * This TM shall be kept confidential and shall not be made public or made available or disclosed to any unauthorized person.
  * Any dispute or claim arising out of the breach of these provisions shall be governed by and construed in accordance with the laws of Hungary.
@@ -18,6 +18,7 @@ import hu.progmasters.settlers.buildings.BuildingType;
 import hu.progmasters.settlers.resources.Resource;
 import hu.progmasters.settlers.resources.TypeOfResources;
 import hu.progmasters.settlers.units.Unit;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +34,7 @@ public class SettlersEngine {
 
     public void build(String typeOfBuilding) {
         BuildingType buildingType = convertStringToEnum(typeOfBuilding);
-        if(buildingType != null) {
+        if (buildingType != null) {
             switch (buildingType) {
                 case ARCHERY:
                     buildings.add(new Archery());
@@ -62,14 +63,14 @@ public class SettlersEngine {
     private void buildingHandler() {
         for (Building building : buildings) {
 
-            if(building.isCanProduce()) {
+            if (building.isCanProduce()) {
                 building.decreaseTurnsLeftForProducing();
-                if(building.getTurnsToProductUnit() == 0) {
+                if (building.getTurnsToProductUnit() == 0) {
                     building.setTurnsToProductUnit();
                     building.produceUnit();
 
                 }
-                if(building.getTurnsToProductResource() == 0) {
+                if (building.getTurnsToProductResource() == 0) {
                     building.setTurnsToProductResource();
                     building.produceResource();
 
@@ -87,35 +88,47 @@ public class SettlersEngine {
         steelQuantity = 0;
         archerQuantity = 0;
         swordsmanQuantity = 0;
+        String buildingInfo = "";
         for (Building building : buildings) {
             setResourceQuantity(building);
-            System.out.println(building);
+            buildingInfo += "--" + building + "\n";
             setUnitQuantity(building);
         }
-
-        System.out.println("Gold: " + goldQuantity);
-        System.out.println("Steel: " + steelQuantity);
-        System.out.println("Archers: " + archerQuantity);
-        System.out.println("Swordsman: " + swordsmanQuantity);
+        printAllInformation(buildingInfo);
     }
 
-    private void setResourceQuantity(Building building) {
-        if(building.getResourceType().equals(TypeOfResources.GOLD)) {
+    private void printAllInformation(String buildingInfo) {
+        System.out.println("Treasury:");
+        System.out.println("--Gold: " + goldQuantity);
+        System.out.println("--Steel: " + steelQuantity);
+        System.out.println("Buildings:");
+        System.out.print(buildingInfo);
+        System.out.println("Units:");
+        if (archerQuantity + swordsmanQuantity == 0) {
+            System.out.println("N/A");
+        } else {
+            if (swordsmanQuantity != 0) System.out.println("Archers: " + archerQuantity);
+            if (archerQuantity != 0) System.out.println("Swordsman: " + swordsmanQuantity);
+        }
+    }
+
+    public void setResourceQuantity(Building building) {
+        if (building.getResourceType().equals(TypeOfResources.GOLD)) {
             goldQuantity += building.getResourceQuantity();
         } else if (building.getResourceType().equals(TypeOfResources.STEEL)) {
             steelQuantity += building.getResourceQuantity();
         }
     }
 
-    private void setUnitQuantity(Building building) {
-        if(building.getBuildingType().equals(BuildingType.ARCHERY)) {
+    public void setUnitQuantity(Building building) {
+        if (building.getBuildingType().equals(BuildingType.ARCHERY)) {
             archerQuantity += building.getUnits().size();
         } else if (building.getBuildingType().equals(BuildingType.BARRACK)) {
             swordsmanQuantity += building.getUnits().size();
         }
     }
 
-    private BuildingType convertStringToEnum(String typeOfHome) {
+    public BuildingType convertStringToEnum(String typeOfHome) {
         for (BuildingType value : BuildingType.values()) {
             if (value.getStringValue().equals(typeOfHome)) {
                 return value;
