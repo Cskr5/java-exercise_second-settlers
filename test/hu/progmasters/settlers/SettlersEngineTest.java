4477c1ab2a9e92d6e5ba175a1115286d4d9b0b66
kristof.csefalvay@gmail.com <kristof.csefalvay@gmail.com>
2020-06-15 12:50:33 +0200

    added tests for creating units

--- test/hu/progmasters/settlers/SettlersEngineTest.java
+++ test/hu/progmasters/settlers/SettlersEngineTest.java
@@ -11,6 +11,61 @@
 
 package hu.progmasters.settlers;
 
+import hu.progmasters.settlers.buildings.Archery;
+import hu.progmasters.settlers.buildings.Barrack;
+import hu.progmasters.settlers.buildings.Building;
+import hu.progmasters.settlers.units.Unit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SettlersEngineTest {
+
+    @BeforeEach
+    public void init() {
+
+    }
+
+    @Test
+    public void test_archery_produceUnit() {
+
+        Archery archery = new Archery();
+
+        archery.turnOnProduction();
+
+        archery.produceUnit();
+        archery.produceUnit();
+        archery.produceUnit();
+
+        Assertions.assertEquals(3,archery.getArchers().size());
+    }
+
+    @Test
+    public void test_barrack_turnsToProdcue() {
+
+        Barrack barrack = new Barrack();
+
+        barrack.turnOnProduction();
+
+        barrack.produceUnit();
+        barrack.produceUnit();
+        barrack.produceUnit();
+        barrack.produceUnit();
+        barrack.produceUnit();
+
+        Assertions.assertEquals(5,barrack.getSwordsmen().size());
+    }
+
+    @Test
+    public void test_archery_produceResource() {
+
+
+    }
+
+    @Test
+    public void test_barrack_produceResource() {
+
+
+    }
 
 }
