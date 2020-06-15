89ae9fcf48b2e4b5a03de19354ca4e65b4fa521a
kristof.csefalvay@gmail.com <kristof.csefalvay@gmail.com>
2020-06-15 15:20:50 +0200

    added UserInterface

--- src/hu/progmasters/settlers/UserInterface.java
+++ src/hu/progmasters/settlers/UserInterface.java
@@ -1,7 +1,33 @@
 package hu.progmasters.settlers;
 
+import hu.progmasters.settlers.engine.SettlersEngine;
+
 import java.util.Scanner;
 
 public class UserInterface {
     Scanner scan = new Scanner(System.in);
+    SettlersEngine settlersEngine = new SettlersEngine();
+
+    public void start() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String cmd = scan.nextLine();
+            switch (cmd) {
+                case "status":
+                    settlersEngine.status();
+                    break;
+                case "skip":
+                    settlersEngine.skip();
+                    break;
+                case "quit":
+                    isRunning = false;
+                    break;
+                default:
+                    String[]  command = cmd.split(" ");
+                    cmd = command[1];
+                    settlersEngine.build(cmd);
+            }
+        }
+    }
 }
